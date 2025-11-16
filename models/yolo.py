c1b532bc42d461cc311d0d01fac5cbbee2c993a2
Your Name <you@example.com>
2025-11-17 00:11:10 +0700

    update

--- models/yolo.py
+++ models/yolo.py
@@ -29,7 +29,7 @@ except:
 from models.spconv import SPYOLOv5Head, SPYOLOv6Head
 from models.experimental import *
 from utils.autoanchor import check_anchor_order
-from utils.general import make_divisible, check_file, set_logging, xyxy2xywh
+from utils.general import make_divisible, check_file, set_logging, xyxy2xywh, colorstr
 from utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
     select_device, copy_attr
 
